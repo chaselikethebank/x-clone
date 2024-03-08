8b8327693e0cfff9a9cea051dfb766f3f9bfbf08
Chase <chasedemaster.tech@gmail.com>
2024-03-08 13:55:23 -0600

    mongoose and singleton stuff

--- database.js
+++ database.js
@@ -4,6 +4,8 @@ const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 const dbName = process.env.DB_NAME;
 const uri = `mongodb+srv://${username}:${password}@cluster0.wbznb0b.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=Cluster0`;
+mongoose.set('useFindAndModify', false)
+mongoose.set('useUnifiedTopology', true)
 
 class Database {
   constructor() {
