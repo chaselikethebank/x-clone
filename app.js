10cfdd29c23186adb7d5ceeb3dddd566412805fc
Chase <chasedemaster.tech@gmail.com>
2024-03-08 13:50:34 -0600

    seperation of db and server

--- app.js
+++ app.js
@@ -4,23 +4,10 @@ const port = 3004;
 const middleware = require('./middleware')
 const path = require('path')
 const bodyParser = require("body-parser")
-const mongoose = require("mongoose");
-require('dotenv').config();
-const username = process.env.DB_USERNAME;
-const password = process.env.DB_PASSWORD;
-const dbName = process.env.DB_NAME;
-const uri = `mongodb+srv://${username}:${password}@cluster0.wbznb0b.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=Cluster0`;
+const mongoose = require("./database");
+ 
 
 
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log("Username:", username);
-        console.log("Database connection successful");
-    })
-    .catch((err) => {
-        console.error("Database connection error:", err);
-        console.log("Username:", username); 
-    });
 
 
 const server = app.listen(port, () => console.log("Server listening on port " + port));
---
+++ database.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+const dbName = process.env.DB_NAME;
+const uri = `mongodb+srv://${username}:${password}@cluster0.wbznb0b.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=Cluster0`;
+
+class Database {
+  constructor() {
+    this.connect();
+  }
+
+  connect() {
+    mongoose
+      .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+      .then(() => {
+        console.log("Username:", username);
+        console.log("Database connection successful");
+      })
+      .catch((err) => {
+        console.error("Database connection error:", err);
+        console.log("Username:", username);
+      });
+  }
+}
+
+
+module.exports = new Database()
